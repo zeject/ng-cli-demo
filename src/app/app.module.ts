6397c0565b28ff4a8ac10a68d529a7476a952e26
zeject <424049902@qq.com>
2016-12-16 22:12:07 +0800

    reove app.module pageC import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AppComponent } from './app.component';
 import { PageAComponent } from './page-a/page-a.component';
 import { PageBComponent } from './page-b/page-b.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageAComponent,
-    PageBComponent,
-    ChildComponent
+    PageBComponent
   ],
   imports: [
     BrowserModule,
