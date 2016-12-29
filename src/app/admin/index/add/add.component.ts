26af77c8e16881439205d984d20a646319e86e94
zeject <424049902@qq.com>
2016-12-29 13:40:36 +0800

    http final

--- src/app/admin/index/add/add.component.ts
+++ src/app/admin/index/add/add.component.ts
@@ -8,6 +8,28 @@ import { IndexService } from '../index.service';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+  tag: boolean = false;
+  userid: string;
+  id: number;
+  phone: string;
+
+  buyerData = {
+    id: this.id,
+    userid: this.userid || '0',
+    data: [{
+      cp_name: '',
+      cp_guige: '',
+      cp_buycount: '',
+      cp_danwei: '',
+      cp_pinpai: '',
+      freight: 0,
+      tax: false,
+      edate: '',
+      note: '',
+      gys: []
+    }]
+  }
+
 
   constructor(
     private route: ActivatedRoute,
@@ -15,10 +37,90 @@ export class AddComponent implements OnInit {
     private service: IndexService) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    let userid = this.route.snapshot.params['userid'];
-    let phone = this.route.snapshot.params['phone'];
-    console.log(id, userid, phone);
+    this.buyerData.id = this.id = +this.route.snapshot.params['id'];
+    this.buyerData.userid = this.userid = this.route.snapshot.params['userid'];
+    this.phone = this.route.snapshot.params['phone'];
+    console.log(this.id, this.userid, this.phone);
+    this.service.getImgBuyerData(this.id).subscribe(res => {
+      if (res.tag) {
+        return;
+      }
+      console.log(res);
+      this.buyerData.data = res.data;
+      this.tag = true;
+    });
+  }
+
+  pushMsg() {
+    this.service.pushMsg(this.id, this.phone).subscribe(res => {
+      if (res.data) {
+        alert('保存成功!');
+      } else {
+        alert('保存失败,请联系管理员');
+      }
+    });
+  }
+
+  del(items, index) {
+    if (!confirm('确定要删除此信息吗?删除后无法恢复!')) {
+      return;
+    }
+    items.splice(index, 1);
+    if (this.buyerData.data.length < 1) {
+      this.addPro();
+    }
+  }
+
+  submit(b) {
+    console.log(this.buyerData);
+    if (this.tag) {
+      // update
+      this.service.updateBuyer(this.buyerData).subscribe(res => {
+        if (res.data) {
+          if (b) this.pushMsg();
+          alert('修改成功!');
+        } else {
+          alert('修改失败!,请联系管理员');
+        }
+      });
+    } else {
+      // add
+      this.service.submitBuyer(this.buyerData).subscribe(res => {
+        if (res.data) {
+          if (b) this.pushMsg();
+          alert('保存成功!');
+          this.tag = true;
+        } else {
+          alert('保存失败!,请联系管理员');
+        }
+      });
+    }
+  }
+
+  addPro() {
+    this.buyerData.data.push({
+      cp_name: '',
+      cp_guige: '',
+      cp_buycount: '',
+      cp_danwei: '',
+      cp_pinpai: '',
+      freight: 0,
+      tax: false,
+      edate: '',
+      note: '',
+      gys: []
+    });
+  }
+
+  addGYS(items) {
+    items.push({
+      gys_shouji: '',
+      gys_xingming: '',
+      gys_gongsi: '',
+      gys_userid: '',
+      gys_baojia: '',
+      note: ''
+    });
   }
 
 }
--- src/app/admin/index/index.service.ts
+++ src/app/admin/index/index.service.ts
@@ -22,4 +22,22 @@ export class IndexService {
         let url = `/admin/zhaohuo/flag/${id}?flag=1`;
         return this.http.get(url).map((res: Response) => res.json());
     }
+
+    getImgBuyerData(id: number) {
+        let url = `/admin/zhaohuo/data/${id}`;
+        return this.http.get(url).map((res: Response) => res.json());
+    }
+
+    pushMsg(id: number, phone: string) {
+        let url = `/admin/zhaohuo/sendmsg?phone=${phone}&id=${id}`;
+        return this.http.get(url).map((res: Response) => res.json());
+    }
+    updateBuyer(obj: any) {
+        let url = `/admin/zhaohuo/upload`;
+        return this.http.put(url, obj).map((res: Response) => res.json());
+    }
+    submitBuyer(obj: any) {
+        let url = `/admin/zhaohuo/upload`;
+        return this.http.post(url, obj).map((res: Response) => res.json());
+    }
 }
